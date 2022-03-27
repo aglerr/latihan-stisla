85a6125c873b22ba86087ed7129cc2fded418e90
mdaffa48 <work@muhammaddaffa.com>
2022-03-27 09:35:00 +0700

    Removing logo image

--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -1,20 +1,20 @@
 @extends('master')
 
+@section('title', 'Peduli Diri - Register')
+
 @section('content')
 <section class="section">
     <div class="container mt-5">
       <div class="row">
         <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
-          <div class="login-brand">
-            <img src="../assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
-          </div>
 
           <div class="card card-primary">
             <div class="card-header"><h4>Register</h4></div>
 
             <div class="card-body">
               <form method="POST" action="/simpanUser">
-                  {{ csrf_field() }}
+                @csrf
+                
                 <div class="form-group">
                   <label for="nik">NIK</label>
                   <input id="nik" type="text" class="form-control" name="nik" tabindex="1" autofocus>
